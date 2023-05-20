dc716946f7759020f24bb86744c1bb567277b912
Ramazan <200107095@stu.sdu.edu.kz>
2023-05-20 23:14:17 +0600

    Добавлено Search Bar

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,6 +6,16 @@ function App() {
     <div className="App">
       <Header />
       <Welcome />
+
+      <section>
+        <div className="card">
+          <h1 className="card_h1">All Stuff</h1>
+          <div className="search">
+            <img src="/images/search.svg" alt="search_logo" />
+            <input className="search_place" placeholder="Search..." />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
